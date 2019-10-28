8d983e90bb86301f6f818aa884f096ad4d49f8fe
Felipe <luisfelipecc@ufps.edu.co>
2019-10-28 17:16:39 -0500

    v3

--- hojaVidaWeb/src/co/hojavida/controller/LoginController.java
+++ hojaVidaWeb/src/co/hojavida/controller/LoginController.java
@@ -37,6 +37,7 @@ public class LoginController extends HttpServlet implements Serializable {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		String email=request.getParameter("email");
+		
 	}
 
 	/**
